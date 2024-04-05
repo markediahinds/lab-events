6db6c59d381b58200b8465974e0f22751edb204e
Markedia Hinds <mhinds@pursuit.org>
2024-04-05 14:03:27 -0400

    M.O.K.A. Pixel Painter Complete

---
+++ pixel-painter/script.js
@@ -0,0 +1,43 @@
+const colors = document.querySelectorAll('.color');
+const canvas = document.querySelector('#canvas');
+const currColor = document.querySelector('#current-color');
+let cells;
+
+console.log(cells, 'hello')
+
+for (let color of colors) {
+    color.addEventListener('click', () => {
+        currColor.style.background = color.style.background;
+    })
+}
+
+function addCell () {
+    for (let i = 0; i < 80; i++) {
+        const cell = document.createElement('div');
+        cell.setAttribute('class', 'cell'); 
+        canvas.append(cell);
+    }
+    cells = document.querySelectorAll('.cell');
+}
+
+addCell()
+
+
+for (let cell of cells) {
+    cell.addEventListener('click', () => {
+        cell.style.background = currColor.style.background;
+    })
+}
+
+
+
+// Working on one color to ensure I could make it work 
+// const color = document.querySelector('.color');
+//     color.addEventListener('click', () => {
+//         const currColor = document.querySelector('#current-color');
+//         currColor.style.background = color.style.background;
+// })
+
+// CSS styling for DOM
+// cell.style.cssText = "border: 1px solid #393e41; box-sizing: border-box; display: inline-block; height: 50px; width: 50px;";
+
--- readme.md
+++ readme.md
@@ -10,9 +10,9 @@ Now that you know how to work with event listeners, you can build a simple pixel
 
 1. Fork and clone this repository.
 
-1. Navigate to the `settings` tab on GitHub, then choose `Pages` from the menu. Configure the `Build and Deployment` to have a `Source` of `Deploy from a branch` and select the `main` branch for deployment. Deployments can take a few minutes, so get started on the lab, and then be sure to check the deployment after you have made a few commits.
+2. Navigate to the `settings` tab on GitHub, then choose `Pages` from the menu. Configure the `Build and Deployment` to have a `Source` of `Deploy from a branch` and select the `main` branch for deployment. Deployments can take a few minutes, so get started on the lab, and then be sure to check the deployment after you have made a few commits.
 
-1. Open up the repository in VSCode. Follow the instructions below to complete the Lab.
+3. Open up the repository in VSCode. Follow the instructions below to complete the Lab.
 
 ## Instructions
 
